b0c922dc9573f0d38285d7175eab43fd4774bf0d
Zubair <zubairaltaf@gmail.com>
2023-08-04 22:07:45 +0530

    done success

---
+++ additionjava.js
@@ -0,0 +1,7 @@
+var num1;
+var num2;
+var res;
+
+document.write("Enter first number");
+
+
---
+++ first_project.py
@@ -0,0 +1,11 @@
+print("hello")
+if(3 < 5): 
+    print ("3 is greater than  5")
+else:
+    print("3 is smaller than 5")
+    
+for i in range :
+    print("loop")
+    
+
+
