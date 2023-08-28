7731be8b82cfb21688945232ccd83975b35e715f
Zubair <zubairaltaf@gmail.com>
2023-08-28 16:46:32 +0530

    New files added

---
+++ function/demo.js
@@ -0,0 +1,23 @@
+  // let x = ()=>{
+            
+        // }
+        let arrobj = {
+            firstName : "Zahid Sir",
+            lastName : "Neaka",
+            address : "Eidgah",
+            profession : "Developer"
+        }
+            // arr:["12,.234,25,1,12,2"],
+            // fun:x,
+            // innerObj:{
+            //     student1:"Zubair",
+            //     student2:"Zaid",
+            //     student3:"Kafeel",
+            //     student4:"Kaheel",
+            //     student5:"Tabish",
+            // }
+        //}
+
+
+        console.log(arrobj.firstName);
+        arrobj.forEach((item) => console.log(item));
